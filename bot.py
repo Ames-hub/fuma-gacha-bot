26cc5ac736b64142a3cfb7e9953e7bdd025c076e
Amelia <114128556+Ames-hub@users.noreply.github.com>
2025-06-30 03:38:18 +0800

    Improved debugging

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from library.database import database
 from library.botapp import botapp
+import os
 
 # Keep DB Up to date
 database.modernize()
@@ -12,4 +13,7 @@ botapp.load_extensions_from("cogs/other")
 botapp.d['maintainer'] = 913574723475083274
 botapp.d['admin_roles'] = [1386536393085419664, 1386536159185862768]
 
-botapp.run()
+DEBUG = os.environ.get("DEBUG").lower() == "true"
+botapp.d['DEBUG'] = DEBUG
+
+botapp.run(shard_count=5 if not DEBUG else 1)
